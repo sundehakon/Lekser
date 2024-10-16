433e3208a7d48c072a22881b0e150b47e659365a
Håkon <hakon.su@gmail.com>
2024-10-16 10:31:16 +0200

    Write functions in modules, implement CLI for better user experience

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,89 @@
+use std::io;
+use modules::readjson::Lekse;
+use colored::Colorize;
+mod modules;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Velkommen til lekser!");
+    loop {
+        println!("Velg et alternativ:");
+        println!("1. Legg til ny lekse");
+        println!("2. Alle lekser");
+        println!("3. Fullfør lekse");
+        println!("4. Fjern lekse");
+        println!("5. Avslutt"); 
+
+        let mut valg = String::new();
+        io::stdin().read_line(&mut valg).expect("Kunne ikke lese linje.");
+
+        match valg.trim() {
+            "1" => {
+                let new_lekse = modules::readjson::get_user_input();
+
+                let filename = "src/data.json".to_string();
+                let mut lekser: Vec<Lekse> = modules::readjson::read_json_file(filename.clone());
+                lekser.push(new_lekse);
+                modules::readjson::write_json_file(filename, &lekser);
+            }
+            "2" => {
+                let filename = "src/data.json".to_string();
+                let lekser: Vec<Lekse> = modules::readjson::read_json_file(filename);
+
+                if lekser.is_empty() {
+                    println!("Ingen lekser tilgjengelig.");
+                } else {
+                    println!("Dine lekser:");
+                    for (index, lekse) in lekser.iter().enumerate() {
+                        if lekse.status {
+                            println!("{}. {}", index + 1, lekse.navn.green());
+                        } else {
+                            println!("{}. {}", index + 1, lekse.navn.red());
+                        }
+                    }
+                }
+            }
+            "3" => {
+
+            }
+            "4" => {
+                let filename = "src/data.json".to_string();
+                let lekser: Vec<Lekse> = modules::readjson::read_json_file(filename.clone());
+                
+                if lekser.is_empty() {
+                    println!("Ingen lekser å fjerne.");
+                } else {
+                    println!("Dine lekser:");
+                    for (index, lekse) in lekser.iter().enumerate() {
+                        if lekse.status {
+                            println!("{}. {}", index + 1, lekse.navn.green());
+                        } else {
+                            println!("{}. {}", index + 1, lekse.navn.red());
+                        }
+                    }
+                }
+
+                println!("Skriv inn tall på lekse du har fullført:");
+
+                let mut index = String::new();
+                io::stdin().read_line(&mut index).expect("Kunne ikke lese linje.");
+
+                let index: usize = match index.trim().parse::<usize>() {
+                    Ok(num) => num - 1,
+                    Err(_) => {
+                        println!("Ugyldig input.");
+                        continue;
+                    }
+                };
+
+                modules::readjson::remove_lekse_by_index(filename, index);
+            }
+            "5" => {
+                println!("Adjø!");
+                break;
+            }
+            _ => {
+                println!("Feil input, prøv igjen.");
+            }
+        } 
+    }
 }
---
+++ src/modules/readjson.rs
@@ -0,0 +1,58 @@
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+use std::io;
+
+#[derive(Serialize, Deserialize)]
+pub struct Lekse {
+    pub navn: String,
+    pub status: bool,
+}
+
+pub fn read_json_file<T: for<'de> Deserialize<'de>>(filename: String) -> Vec<T> {
+    let path = Path::new(&filename);
+    
+    if !path.exists() {
+        println!("Fant ikke fil, begynner på ny tom liste.");
+        return vec![];
+    }
+
+    let mut fdata = String::new();
+    let mut rfile = File::open(path).expect("Kan ikke åpne fil.");
+    rfile.read_to_string(&mut fdata).expect("Kan ikke lese fil.");
+    
+    serde_json::from_str(&fdata).unwrap_or_else(|_| vec![])
+}
+
+pub fn write_json_file<T: Serialize>(filename: String, data: &Vec<T>) {
+    let mut file = File::create(filename).expect("Kan ikke lage fil.");
+    let json = serde_json::to_string_pretty(&data).expect("Kan ikke serialisere data.");
+    file.write_all(json.as_bytes()).expect("Kan ikke skrive data til fil.");
+
+    println!("Data oppdatert!");
+}
+
+pub fn get_user_input() -> Lekse {
+    println!("Skriv inn lekse:");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Kunne ikke lese linje.");
+
+    Lekse {
+        navn: input.trim().to_string(),
+        status: false,
+    }
+}
+
+pub fn remove_lekse_by_index(filename: String, index: usize) {
+    let mut lekser: Vec<Lekse> = read_json_file(filename.clone());
+
+    if index >= lekser.len() {
+        println!("Ugyldig index.");
+        return;
+    }
+
+    lekser.remove(index);
+    write_json_file(filename, &lekser);
+}
